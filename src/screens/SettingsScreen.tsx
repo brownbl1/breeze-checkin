90a60ddbaca92bc8c7ac41dad83fece7d7543acb
Ben Brown <itslittlejohn@gmail.com>
2019-07-19 16:17:40 -0400

    Settings Screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import { NavigationScreenProps } from 'react-navigation'
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { getPrinter } from '../helpers/getPrinter'
 
-import { styles } from './styles'
+const mapState = ({ printer }) => ({ printer })
 
-export const SettingsScreen: React.FC<NavigationScreenProps> = () => {
+const mapDispatch = dispatch => ({
+  onPress: async () => {
+    const printer = await getPrinter(true)
+    if (printer) {
+      dispatch.printer.select(printer)
+    }
+  },
+})
+
+type Props = {} & ReturnType<typeof mapState> & ReturnType<typeof mapDispatch>
+
+const ScreenContents: React.FC<Props> = ({ onPress, printer }) => {
+  const printerName = printer && printer.name
+
   return (
-    <View style={styles.container}>
-      <Text>SettingsScreen</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={onPress} title="Select Printer" />
+      <Text style={{ marginTop: 10 }}>{printerName}</Text>
     </View>
   )
+}
+
+const ConnectedContents = connect(
+  mapState,
+  mapDispatch
+)(ScreenContents)
+
+export const SettingsScreen: React.FC = () => {
+  return <ConnectedContents />
 }
