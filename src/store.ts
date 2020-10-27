c37c00125444a051ef6a94bf1085524bc5b8e2d2
Ben Brown <itslittlejohn@gmail.com>
2020-10-27 11:24:26 -0400

    simplify store

--- src/store.ts
+++ src/store.ts
@@ -16,35 +16,40 @@ export const store = init<RootModel>({
   ],
 })
 
+store.subscribe(
+  watch(
+    store.getState,
+    'settings.date',
+  )((newVal, oldVal, path) => {
+    console.log('%s changed from %s to %s', path, oldVal, newVal)
+
+    store.dispatch.events.selectEntrustEventAsync()
+    store.dispatch.events.selectTeacherEventAsync()
+  }),
+)
+
+store.subscribe(
+  watch(
+    store.getState,
+    'settings.entrustEventId',
+  )((newVal, oldVal, path) => {
+    console.log('%s changed from %s to %s', path, oldVal, newVal)
+
+    store.dispatch.events.selectEntrustEventAsync()
+  }),
+)
+
+store.subscribe(
+  watch(
+    store.getState,
+    'settings.teacherEventId',
+  )((newVal, oldVal, path) => {
+    console.log('%s changed from %s to %s', path, oldVal, newVal)
+
+    store.dispatch.events.selectTeacherEventAsync()
+  }),
+)
+
 export type Store = typeof store
 export type Dispatch = RematchDispatch<RootModel>
 export type RootState = RematchRootState<RootModel>
-
-const watchDate = watch(store.getState, 'settings.date')
-const watchEntrustEventId = watch(store.getState, 'settings.entrustEventId')
-const watchTeacherEventId = watch(store.getState, 'settings.teacherEventId')
-
-store.subscribe(
-  watchDate((newVal, oldVal, path) => {
-    console.log('%s changed from %s to %s', path, oldVal, newVal)
-
-    store.dispatch.events.selectEntrustEventAsync()
-    store.dispatch.events.selectTeacherEventAsync()
-  }),
-)
-
-store.subscribe(
-  watchEntrustEventId((newVal, oldVal, path) => {
-    console.log('%s changed from %s to %s', path, oldVal, newVal)
-
-    store.dispatch.events.selectEntrustEventAsync()
-  }),
-)
-
-store.subscribe(
-  watchTeacherEventId((newVal, oldVal, path) => {
-    console.log('%s changed from %s to %s', path, oldVal, newVal)
-
-    store.dispatch.events.selectTeacherEventAsync()
-  }),
-)
